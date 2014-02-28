90477dbe15a07164573fd29e2e65939ebe61af57
Maciej Orliński <maciejorlinski1@gmail.com>
2014-02-28 18:37:41 +0100

    Logowanie i inne
    No generalnie zmienilem mechanizm logowania: skrypt prosi o dane w
    momencie logowania. Plus kilka poprawek, m. in. globalny verbose

--- uw.py
+++ uw.py
@@ -1,23 +1,26 @@
 from twill.commands import *
 import paramiko
+from getpass import getpass
 
 #Klasa do poruszania sie po usosie
 class Usos():
-	def __init__(self,login,password):
-		self.login=login
-		self.password=password
+	def __init__(self):
 		self.loggedIn=False
-
+    
 	#metoda logujaca
 	def logIn(self):
+		#Wiem, global, brzydko ale trudno. Jeden global na skrypt ujdzie
+		global verbose
+		self.login=raw_input('PESEL: ')
+		self.password=getpass()
 		go('https://logowanie.uw.edu.pl/cas/login')
 		fv('1','username',self.login)
 		fv('1','password',self.password)
 		try:
 			dummy=submit('3')
 			self.loggedIn=True
-
-			#Easier to ask for forgiveness than permission
+            
+			#"Easier to ask for forgiveness than permission"
 			try:
 				verbose
 			except NameError:
@@ -38,20 +41,25 @@ class Usos():
 
 #Klasa do poruszania sie po serwerze mimuw
 class Ssh():
-	def __init__(self, login, password):
-		self.login=login
-		self.password=password
+	def __init__(self):
 		self.host="students.mimuw.edu.pl"
 		self.port=22
 		self.loggedIn=False
 		self.mailList=[]
-
+        
 		self.client=paramiko.SSHClient()
 		self.client.load_system_host_keys()
 		self.client.set_missing_host_key_policy(paramiko.WarningPolicy)
-
-	def logIn(self):
+    
+	def logIn(self, pw=None):
+		global verbose
 		try:
+			self.login=raw_input('login: ')
+			if not pw:
+				self.password=getpass()
+			else:
+				self.password=pw
+            
 			self.client.connect(self.host, username=self.login, password=self.password, port=self.port)
 			self.loggedIn=True
 			try:
@@ -62,7 +70,7 @@ class Ssh():
 				print "Ssh - zalogowano."
 		except:
 			print "Error 20: Logowanie nie powiodlo sie."
-
+    
 	def execute(self, command):
 		if self.loggedIn:
 			stdin, stdout, stderr = self.client.exec_command(command)
@@ -86,13 +94,13 @@ class Ssh():
 					print "Nowa poczta!"
 				mailList=stdoutCopy.split('\n')
 				mailCount=1
-
+                
 				for mail in mailList:
 					if mail!='':
 						print mailCount, mail
 						self.mailList.append(mail)
 					mailCount+=1
-
+            
 			else:
 				try:
 					verbose
@@ -100,7 +108,7 @@ class Ssh():
 					verbose=True
 				if verbose:
 					print "Nie ma nowych wiadomosci"
-
+    
 	def displayMail(self):
 		if self.loggedIn:
 			if self.mailList==[]:
@@ -124,37 +132,38 @@ class Ssh():
 if __name__=="__main__":
 	import argparse
 	from os import remove
-
+    
 	parser=argparse.ArgumentParser()
-	parser.add_argument("--usosLogin", "-ul")
-	parser.add_argument("--sshLogin", "-sl")
-	parser.add_argument("--password", "-pw")
 	
 	parser.add_argument("--verbose", "-v", action="store_true")
 	parser.add_argument("--usos", "-u", action="store_true")
 	parser.add_argument("--ssh", "-s", action="store_true")
 	parser.add_argument("--mail", "-m", action="store_true")
-
+    
 	args=parser.parse_args()
-
+    
 	verbose=args.verbose
-
+    
 	redirect_output('dummy')
-
+    
 	if args.usos:
 		try:
-			UsosClient=Usos(args.usosLogin, args.password)
+			UsosClient=Usos()
 		except NameError:
 			print "Error 42: Nie podano loginu lub hasla do usosa"
 		UsosClient.logIn()
 	if args.ssh:
 		try:
-			SshClient=Ssh(args.sshLogin, args.password)
+			SshClient=Ssh()
 		except NameError:
 			print "Error 43: Nie podano loginu lub hasla do ssh"
-		SshClient.logIn()
+		try:
+			SshClient.logIn(pw=UsosClient.password)
+		except NameError:
+			SshClient.logIn()
 		if args.mail:
 			SshClient.checkMail()
-#			SshClient.displayMail()
-
+    #TODO: regex albo cos co pomoze wyciagnac tresc mejla i przedstawic go w czytelny sposob
+    #			SshClient.displayMail()
+    
 	remove('dummy')
