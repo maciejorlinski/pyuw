df3f4b10c1b0de3f9d3025f49c41ec87e9e9327e
Maciej Orliński <maciejorlinski1@gmail.com>
2014-03-02 21:19:52 +0100

    Plik config.py + nowe parametry Dodalem plik config.py zawierajacy domyslne wartosci parametrow i zmodyfikowalem odpowiednio kod. Dodalem tez opcje podania peselu, loginu i hasla w parametrach

---
+++ config.py
@@ -0,0 +1,11 @@
+#Odkomentuj linijki aby ustawic domyslna wartosc parametrow
+
+vars={
+#'verbose': True,
+#'usos': True,
+#'ssh': True,
+#'mail': True,
+#'pesel': '',
+#'login': '',
+#'password': ''
+}
